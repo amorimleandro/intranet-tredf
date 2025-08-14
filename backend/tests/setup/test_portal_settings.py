a54ac081614ad13afef3bd42ac38159e6c11ea34
Leandro Amorim <leandro.carisio@tre-df.jus.br>
2025-08-14 16:07:37 -0300

    Exercício 17: Testes de configuração do backend

---
+++ backend/tests/setup/test_portal_settings.py
@@ -0,0 +1,25 @@
+"""Portal settings tests."""
+
+from plone import api
+
+import pytest
+
+
+class TestPortalSettings:
+    """Test that Portal configuration is correctly done."""
+
+    @pytest.mark.parametrize(
+        "key,expected",
+        [
+            ["plone.site_title", "Intranet do TRE-DF"],
+            ["plone.email_from_name", "Intranet do TRE-DF"],
+            ["plone.smtp_host", "localhost"],
+            ["plone.smtp_port", 25],
+            ["plone.available_languages", ["pt-br"]],
+            ["plone.twitter_username", "plone"],
+        ],
+    )
+    def test_setting(self, portal, key: str, expected: str):
+        """Test portal title."""
+        value = api.portal.get_registry_record(key)
+        assert value == expected
